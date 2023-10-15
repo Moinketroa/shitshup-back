4f3b4ee6dcbd98c46e449686b5c94cade22cfb9c
jimeme <j-m_deb@hotmail.fr>
2023-10-15 17:13:18 +0200

    Refacto : Shift step numbers

--- src/youtube/process-pending/process-pending.service.ts
+++ src/youtube/process-pending/process-pending.service.ts
@@ -3,11 +3,11 @@ import { YoutubeUser } from '../../auth/youtube-auth/model/youtube-user.model';
 import { Step1Service } from './step-1.service';
 import { Step2Service } from './step-2.service';
 import { Step3Service } from './step-3.service';
-import { Step5Service } from './step-5.service';
+import { Step4Service } from './step-4.service';
 import { ProcessTaskService } from './process-task.service';
 import { Step2Results } from './model/step-2-results.model';
 import { MusicDataAnalysisResult } from './model/music-data-analysis-result.model';
-import { Step6Service } from './step-6.service';
+import { Step5Service } from './step-5.service';
 
 @Injectable({
     scope: Scope.TRANSIENT,
@@ -18,8 +18,8 @@ export class ProcessPendingService {
         private readonly step1: Step1Service,
         private readonly step2: Step2Service,
         private readonly step3: Step3Service,
+        private readonly step4: Step4Service,
         private readonly step5: Step5Service,
-        private readonly step6: Step6Service,
         private readonly processTaskService: ProcessTaskService,
     ) {
     }
@@ -38,9 +38,9 @@ export class ProcessPendingService {
 
         await this.triggerStep3(youtubeUser, allIdsToProcess, step2Results);
 
-        const musicDataAnalysisResults = await this.triggerStep5(step2Results);
+        const musicDataAnalysisResults = await this.triggerStep4(step2Results);
 
-        await this.triggerStep6(musicDataAnalysisResults);
+        await this.triggerStep5(musicDataAnalysisResults);
 
         console.log('[PROCESS_PENDING] Process ended.');
         return {
@@ -76,16 +76,16 @@ export class ProcessPendingService {
         await this.processTaskService.incrementTasksDone();
     }
 
-    private async triggerStep5(step2Results: Step2Results) {
-        const musicDataAnalysisResults = await this.step5.stepGetMusicInfos(step2Results.fileInfos);
+    private async triggerStep4(step2Results: Step2Results) {
+        const musicDataAnalysisResults = await this.step4.stepGetMusicInfos(step2Results.fileInfos);
 
         await this.processTaskService.incrementTasksDone();
 
         return musicDataAnalysisResults;
     }
 
-    private async triggerStep6(musicDataAnalysisResults: MusicDataAnalysisResult[]): Promise<void> {
-        await this.step6.stepPushResultsToNotion(musicDataAnalysisResults);
+    private async triggerStep5(musicDataAnalysisResults: MusicDataAnalysisResult[]): Promise<void> {
+        await this.step5.stepPushResultsToNotion(musicDataAnalysisResults);
 
         await this.processTaskService.incrementTasksDone();
     }
